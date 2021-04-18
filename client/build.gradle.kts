6d468f4ef28f733b3cf737a3d134e0fee3171632
Ekaterina Ledeneva <ekiuled@gmail.com>
2021-04-18 20:53:53 +0300

    Gson dependency

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -9,13 +9,11 @@ repositories {
 
 dependencies {
     testImplementation("junit:junit:4.13")
-
     implementation("org.slf4j:slf4j-api:1.7.+")
     implementation("org.slf4j:slf4j-simple:1.7.+")
     implementation("com.rabbitmq:amqp-client:5.+")
-
+    implementation("com.google.code.gson:gson:2.8.+")
     implementation("com.github.trystan:AsciiPanel:master-SNAPSHOT")
-
     implementation(project(":lib"))
 }
 
