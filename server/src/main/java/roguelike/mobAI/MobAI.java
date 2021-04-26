667e27fe61a199ddebebae12aceeebaf0ae308d6
EliseevEgor <gorgpupkin@gmail.com>
2021-04-26 04:44:31 +0300

    Try new seePlayer

--- server/src/main/java/roguelike/mobAI/MobAI.java
+++ server/src/main/java/roguelike/mobAI/MobAI.java
@@ -6,7 +6,6 @@ import roguelike.util.Action;
 import roguelike.util.Position;
 
 import java.util.Comparator;
-import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -48,15 +47,44 @@ public abstract class MobAI {
     /**
      * Determines whether given position is visible for the mob
      */
+//    protected boolean seePlayer(Position position) {
+//        int mobX = mob.getPosition().getX();
+//        int mobY = mob.getPosition().getY();
+//        int playerX = position.getX();
+//        int playerY = position.getY();
+//        while (playerX != mobX || playerY != mobY) {
+//            mobX -= Math.signum(mobX - playerX);
+//            mobY -= Math.signum(mobY - playerY);
+//            if (!mob.getLevel().isNotWall(new Position(mobX, mobY))) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
     protected boolean seePlayer(Position position) {
-        int mobX = mob.getPosition().getX();
-        int mobY = mob.getPosition().getY();
-        int playerX = position.getX();
-        int playerY = position.getY();
-        while (playerX != mobX || playerY != mobY) {
-            mobX -= Math.signum(mobX - playerX);
-            mobY -= Math.signum(mobY - playerY);
-            if (!mob.getLevel().isNotWall(new Position(mobX, mobY))) {
+        int x0 = mob.getPosition().getX();
+        int y0 = mob.getPosition().getY();
+        int x1 = position.getX();
+        int y1 = position.getY();
+        int deltax = Math.abs(x1 - x0);
+        int deltay = Math.abs(y1 - y0);
+        int error = 0;
+        int deltaerr = deltay + 1;
+        int y = y0;
+        int diry = y1 - y0;
+        if (diry > 0) {
+            diry = 1;
+        }
+        if (diry < 0) {
+            diry = -1;
+        }
+        for (int x = Math.min(x0, x1); x < Math.max(x0, x1); x++) {
+            error += deltaerr;
+            if (error >= (deltax + 1)) {
+                y += diry;
+                error -= (deltax + 1);
+            }
+            if (!mob.getLevel().isNotWall(new Position(x, y))) {
                 return false;
             }
         }
